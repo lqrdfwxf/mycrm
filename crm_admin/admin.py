a3e886d3781f390e9b9f1e7445bcaafc36d03fdd
lqrdfwxf <390667213@qq.com>
2021-01-10 20:19:57 +0800

    第一版基本功能

--- crm_admin/admin.py
+++ crm_admin/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from crm_admin import models
+
 
 # Register your models here.
+
+
+class Seller(admin.ModelAdmin):
+    list_display = ('id', 'sell_name',
+                    'seller_wechat',
+                    'seller_qq',
+                    'seller_phone_number',
+                    'seller_create_time',
+                    'seller_update_time'
+                    )
+
+
+class Product(admin.ModelAdmin):
+    list_display = ('id', 'product_name',
+                    'product_country',
+                    'product_keywords',
+                    'product_sold_by',
+                    'product_asin',
+                    'product_link',
+                    'product_num_per_day',
+                    'product_num_total',
+                    'product_create_time',
+                    'product_update_time',
+                    'product_seller'
+                    )
+
+
+class Buyer(admin.ModelAdmin):
+    list_display = ('id', 'buyer_name',
+                    'buyer_country',
+                    'buyer_paypal',
+                    'buyer_profile',
+                    'buyer_agent',
+                    'buyer_create_time',
+                    'buyer_update_time')
+
+
+class Order(admin.ModelAdmin):
+    list_display = ('id', 'order_number', 'product_id', 'buyer_id', 'order_create_time', 'order_update_time')
+
+
+class Agent(admin.ModelAdmin):
+    list_display = ('id', 'agent_name', 'agent_create_time', 'agent_update_time')
+
+
+class Country(admin.ModelAdmin):
+    list_display = ('id', 'country_name')
+
+
+admin.site.register(models.Seller, Seller)
+admin.site.register(models.Product, Product)
+admin.site.register(models.Buyer, Buyer)
+admin.site.register(models.Order, Order)
+admin.site.register(models.Agent, Agent)
+admin.site.register(models.Country, Country)
--- crm_admin/models.py
+++ crm_admin/models.py
@@ -1,23 +1,57 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class Seller(models.Model):
-    pass
+    sell_name = models.CharField('名称', max_length=16, unique=True)
+    seller_wechat = models.CharField('微信', max_length=64, unique=True)
+    seller_qq = models.CharField('QQ', max_length=64, unique=True)
+    seller_phone_number = models.CharField('手机号码', max_length=64)
+    seller_create_time = models.DateTimeField('创建时间', auto_now_add=True)
+    seller_update_time = models.DateTimeField('修改时间', auto_now=True)
 
 
 class Product(models.Model):
-    pass
+    product_name = models.CharField('名称', max_length=64)
+    product_country = models.ForeignKey('Country', on_delete=models.PROTECT)
+    product_keywords = models.CharField('关键词', max_length=255)
+    product_sold_by = models.CharField('店铺名', max_length=64)
+    product_asin = models.CharField('Asin', max_length=64)
+    product_link = models.CharField('链接', max_length=64)
+    product_num_per_day = models.BigIntegerField('每日单数', )
+    product_num_total = models.BigIntegerField('总单数', )
+    product_create_time = models.DateTimeField('创建时间', auto_now_add=True)
+    product_update_time = models.DateTimeField('修改时间', auto_now=True)
+    product_seller = models.ForeignKey('Seller', on_delete=models.PROTECT)
+
+
+class Agent(models.Model):
+    agent_name = models.CharField('名称', max_length=64)
+    agent_country = models.ManyToManyField('Country')
+
+    agent_create_time = models.DateTimeField('创建时间', auto_now_add=True)
+    agent_update_time = models.DateTimeField('修改时间', auto_now=True)
 
 
 class Buyer(models.Model):
-    pass
+    buyer_name = models.CharField('名称', max_length=64)
+    buyer_country = models.CharField('国家', max_length=64)
+    buyer_paypal = models.EmailField('Paypal', )
+    buyer_profile = models.CharField('亚马逊Profile', max_length=64)
+    buyer_agent = models.OneToOneField(Agent, on_delete=models.PROTECT)
+    buyer_create_time = models.DateTimeField('创建时间', auto_now_add=True)
+    buyer_update_time = models.DateTimeField('修改时间', auto_now=True)
 
 
 class Order(models.Model):
-    pass
+    order_number = models.CharField('订单ID', max_length=64)
+    product_id = models.ForeignKey('Product', on_delete=models.PROTECT)
+    buyer_id = models.ForeignKey('Buyer', on_delete=models.PROTECT)
+    order_create_time = models.DateTimeField('创建时间', auto_now_add=True)
+    order_update_time = models.DateTimeField('修改时间', auto_now=True)
 
 
 class Country(models.Model):
-    country_name = models.CharField(max_length=16,unique=True)
+    country_name = models.CharField('国家', max_length=16, unique=True)
